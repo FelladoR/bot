e531e59ee30f4945dd6f0bd08dbf7e306bb2f17d
FelladoRR <61095224+FelladoRR@users.noreply.github.com>
2024-01-25 01:09:51 +0200

    Fixed warn command

--- cogs/warn.py
+++ cogs/warn.py
@@ -5,7 +5,7 @@ from random import randint
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='-', intents=intents) 
+bot = commands.Bot(command_prefix='>', intents=intents) 
 cluster = MongoClient('mongodb+srv://FelladoR:maxum26072007@cluster0.o9csmz1.mongodb.net/?retryWrites=true&w=majority')
 class Warn(commands.Cog):
     def __init__(self, bot):
@@ -26,16 +26,27 @@ class Warn(commands.Cog):
         user_data = cluster.testbase.collusers.find_one({'_id': member.id, 'guild_id': ctx.guild.id})
         
         if user_data:
-            if user_data['warns'] >= 3:
-                # Reset warns and reasons
+            if user_data['warns'] >=2:
+                # Ресет варнів і попереджень
                 cluster.testbase.collusers.update_one(
                     {'_id': member.id, 'guild_id': ctx.guild.id},
                     {'$set': {'warns': 0, 'reasons': []}}
                 )
+
+                # Create an Embed for the user
+                embed = discord.Embed(
+                    title='Блокування',
+                    description=f'Ви отримали блокування на сервері {ctx.guild.name} Причина: ``максимальна кількість попереджень``.',
+                    color=discord.Color.red()
+                    
+                )
+                # Send the embed to the user's DM channel
+                await member.send(embed=embed)
+
             else:
                 server_data = cluster.testbase.collservers.find_one({'_id': ctx.guild.id})
                 if server_data:
-                    # Increment warns and add reason
+        # Increment warns and add reason
                     cluster.testbase.collservers.update_one(
                         {'_id': ctx.guild.id},
                         {'$inc': {'case': 1}}
@@ -54,6 +65,16 @@ class Warn(commands.Cog):
                         }
                     )
                     await ctx.send(f"{ctx.author} видав попередження {member}, причина: ``{reason}``. Case: {server_data['case']}")
+                    user_data["warns"] += 1
+                    new_warns_value = user_data["warns"]
+                    warningembed = discord.Embed(
+                        title='Повідомлення',
+                        description=f'Ви отримали попередження на сервері {ctx.guild.name} Причина: ``{str(reason)}``. Кількіть ваших попереджень: {new_warns_value}',
+                        color=discord.Color.yellow()
+                    )
+
+                    await member.send(embed=warningembed)
+
                 else:
                     await ctx.send("Server data not found")
         else:
@@ -69,29 +90,6 @@ class Warn(commands.Cog):
                 }]
             })
             await ctx.send(f"{ctx.author} видав перше попередження {member}, причина: ``{reason}``. Case: 1")
-
-    @commands.command(name='warns')
-    async def warns(ctx, member: discord.Member = None):
-        if member is None:
-            member = ctx.author
-
-        usr = cluster.testbase.collusers.find_one({"_id": member.id, "guild_id": ctx.guild.id})
-        if usr and "reasons" in usr:
-            embed = discord.Embed(title="Попередження: " + str(usr['warns']), color=discord.Color.orange())
-            for value in usr["reasons"]:
-                if 'warns' in value and (value['warns'] == 0 or value['warns'] is None):
-                    await ctx.send('Користувач або попередження не знайдені.')
-                    return
-                else:
-                    moderator = await bot.fetch_user(value['author_id'])
-                    embed.add_field(
-                        name=f"Модератор: {moderator}", 
-                        value=f"ID: {value['case']}\n Причина: ``{value['reason']}``", 
-                        inline=False
-                    )
-            await ctx.send(embed=embed)
-        else:
-            await ctx.send("Користувач або попередження не знайдені.")
 
 async def on_command_error(self, ctx, error):
         if isinstance(error, commands.CommandNotFound):
