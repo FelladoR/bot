0ba1b75eb6c952c8d15a00f863b2651339717f44
Максим <maxum2607@gmail.com>
2024-02-09 03:22:32 +0200

    Змінена команда profile

--- cogs/profile.py
+++ cogs/profile.py
@@ -2,50 +2,66 @@ from discord.ext import commands
 import discord
 from pymongo import MongoClient
 from bot import cluster
-cluster = MongoClient('mongodb+srv://FelladoR:maxum26072007@cluster0.o9csmz1.mongodb.net/?retryWrites=true&w=majority')
 
 class Profile(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-     
+
+    async def get_custom_emoji(self, guild, emoji_name):
+        # Your logic to get custom emoji here, assuming guild is a valid Guild object
+        # For example:
+        emoji = discord.utils.get(guild.emojis, name=emoji_name)
+        return str(emoji) if emoji else ''
+
     @commands.command(name='profile')
     async def profile(self, ctx, member: discord.Member = None):
-        if member is None:
-            member = ctx.author
+        try:
+            if member is None:
+                member = ctx.author
 
-        usr = cluster.testbase.collusers.find_one({
-            "_id": member.id,
-            "guild_id": ctx.guild.id
-        })
+            # Use the correct collection and database names (collusers and testbase)
+            usr = cluster.testbase.collusers.find_one({"_id": member.id})
 
-        embed = discord.Embed(title=f'Профіль користувача {member}',
-                              color=discord.Color.random())
+            embed = discord.Embed(title=f'Профіль користувача {member}',
+                                  color=0x2471A3)
 
-        embed.set_thumbnail(url=member.avatar)  # Set user's avatar as thumbnail
+            # Check if member has an avatar
+            avatar_url = member.avatar.url if member.avatar else member.default_avatar.url
+            moneyemoji = await self.get_custom_emoji(ctx.guild, '9243_DiscordCoin')
+            embed.set_thumbnail(url=avatar_url)  # Set user's avatar as thumbnail
 
-        embed.add_field(name='Ім\'я', value=member.name, inline=True)
-        embed.add_field(name='Тег', value=member.discriminator, inline=True)
-        embed.add_field(name='ID', value=f'``{member.id}``', inline=False)
+            embed.add_field(name='Ім\'я', value=member.name, inline=True)
+            embed.add_field(name='Тег', value=member.discriminator, inline=True)
+            embed.add_field(name='ID', value=f'``{member.id}``', inline=False)
 
-        if usr:
-            embed.add_field(
-                name='Інформація про акаунт',
-                value=f'Дата регістрації акаунту: {member.created_at.strftime("%d-%m-%Y %H:%M:%S")}\n'
-                      f'Дата приєднання до гільдії: {member.joined_at.strftime("%d-%m-%Y %H:%M:%S")}\n'
-                      f'Попереджень: {str(usr["warns"])}',
-                inline=False
-            )
-        else:
-            embed.add_field(
-                name='Інформація про акаунт',
-                value=f'Дата регістрації акаунту: {member.created_at.strftime("%d-%m-%Y %H:%M:%S")}\n'
-                      f'Дата приєднання до гільдії: {member.joined_at.strftime("%d-%m-%Y %H:%M:%S")}\n'
-                      f'Попереджень: 0',
-                inline=False
-            )
+            if usr is not None:
+                # Check if "balance" key is present in the document
+                money = usr.get("money", 0)
+                embed.add_field(name='Цінності', value=f'Монетки: {money}{moneyemoji}', inline=False)
 
-        await ctx.send(embed=embed)
+                # Check if "warns" key is present in the document
+                warns = usr.get("warns", 0)
+                embed.add_field(
+                    name='Інформація про акаунт',
+                    value=f'Дата регістрації акаунту: {member.created_at.strftime("%d-%m-%Y %H:%M:%S")}\n'
+                          f'Дата приєднання до гільдії: {member.joined_at.strftime("%d-%m-%Y %H:%M:%S")}\n'
+                          f'Попереджень: {warns}',
+                    inline=False
+                )
+            else:
+                embed.add_field(name='Цінності', value='Гроші: 0', inline=False)
+                embed.add_field(
+                    name='Інформація про акаунт',
+                    value=f'Дата регістрації акаунту: {member.created_at.strftime("%d-%m-%Y %H:%M:%S")}\n'
+                          f'Дата приєднання до гільдії: {member.joined_at.strftime("%d-%m-%Y %H:%M:%S")}\n'
+                          f'Попереджень: 0',
+                    inline=False
+                )
+
+            await ctx.send(embed=embed)
+        except Exception as e:
+            print(f'Помилка у команді profile: {e}')
+            return
 
 async def setup(bot):
-    
     await bot.add_cog(Profile(bot))
